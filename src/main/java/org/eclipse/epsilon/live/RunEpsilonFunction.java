cb224fdef12edc0418c888751459a30ff9006ed1
Dimitris Kolovos <dimitris.kolovos@york.ac.uk>
2023-01-28 23:03:29 +0000

    Added support for EML

--- src/main/java/org/eclipse/epsilon/live/RunEpsilonFunction.java
+++ src/main/java/org/eclipse/epsilon/live/RunEpsilonFunction.java
@@ -6,10 +6,13 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.Map;
 
+import org.eclipse.epsilon.ecl.EclModule;
+import org.eclipse.epsilon.ecl.trace.MatchTrace;
 import org.eclipse.epsilon.egl.EglTemplateFactoryModuleAdapter;
 import org.eclipse.epsilon.egl.EgxModule;
 import org.eclipse.epsilon.egl.IEglModule;
 import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
+import org.eclipse.epsilon.eml.EmlModule;
 import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.IEolModule;
 import org.eclipse.epsilon.eol.execute.context.Variable;
@@ -34,11 +37,13 @@ public class RunEpsilonFunction extends EpsilonLiveFunction {
 				request.get("emfatic").getAsString(), 
 				request.get("secondFlexmi").getAsString(), 
 				request.get("secondEmfatic").getAsString(), 
+				request.get("thirdFlexmi") != null ? request.get("thirdFlexmi").getAsString() : "", 
+				request.get("thirdEmfatic") != null ? request.get("thirdEmfatic").getAsString() : "", 
 				bos, response);
 		response.addProperty("output", bos.toString());
 	}
 	
-	public void run(String language, String program, String secondProgram, String flexmi, String emfatic, String secondFlexmi, String secondEmfatic, OutputStream outputStream, JsonObject response) throws Exception {
+	public void run(String language, String program, String secondProgram, String flexmi, String emfatic, String secondFlexmi, String secondEmfatic, String thirdFlexmi, String thirdEmfatic, OutputStream outputStream, JsonObject response) throws Exception {
 		
 		IEolModule module = createModule(language);
 		module.parse(program, new File("/program." + language));
@@ -56,9 +61,47 @@ public class RunEpsilonFunction extends EpsilonLiveFunction {
 			case "epl": runEpl((EplModule) module, flexmi, emfatic, response); return;
 			case "egl": runEgl((IEglModule) module, flexmi, emfatic, response); return;
 			case "egx": runEgx((EgxModule) module, secondProgram, flexmi, emfatic, response); return;
+			case "eml": runEml((EmlModule) module, secondProgram, flexmi, emfatic, thirdFlexmi, thirdEmfatic, secondEmfatic, response); return;
 			default: runEol((EolModule) module, flexmi, emfatic);
 		}
 		
+	}
+	
+	protected void runEml(EmlModule module, String ecl, String leftFlexmi, String leftEmfatic, String rightFlexmi, String rightEmfatic, String mergedEmfatic, JsonObject response) throws Exception {
+		
+		EclModule eclModule = new EclModule();
+		
+		eclModule.parse(ecl, new File("/program.ecl"));
+		if (!eclModule.getParseProblems().isEmpty()) {
+			response.addProperty("error", eclModule.getParseProblems().get(0).toString());
+			return;
+		}
+		
+		InMemoryEmfModel leftModel = getInMemoryFlexmiModel(leftFlexmi, leftEmfatic);
+		leftModel.setName("Left");
+		leftModel.getAliases().add("Source");
+		
+		InMemoryEmfModel rightModel = getInMemoryFlexmiModel(rightFlexmi, rightEmfatic);
+		rightModel.setName("Right");
+		rightModel.getAliases().add("Source");
+		
+		InMemoryEmfModel mergedModel = getBlankInMemoryModel(mergedEmfatic);
+		mergedModel.setName("Merged");
+		mergedModel.getAliases().add("Target");
+		
+		eclModule.getContext().getModelRepository().addModel(leftModel);
+		eclModule.getContext().getModelRepository().addModel(rightModel);
+		
+		MatchTrace matchTrace = eclModule.execute();
+		
+		module.getContext().setMatchTrace(matchTrace.getReduced());
+		module.getContext().getModelRepository().addModel(leftModel);
+		module.getContext().getModelRepository().addModel(rightModel);
+		module.getContext().getModelRepository().addModel(mergedModel);
+		
+		module.execute();
+		
+		response.addProperty("targetModelDiagram", new FlexmiToPlantUMLFunction().run(mergedModel));
 	}
 	
 	protected void runEtl(EtlModule module, String flexmi, String emfatic, String secondEmfatic, JsonObject response) throws Exception {
@@ -155,6 +198,7 @@ public class RunEpsilonFunction extends EpsilonLiveFunction {
 		case "epl": return new EplModule();
 		case "egl": return new EglTemplateFactoryModuleAdapter();
 		case "egx": return new EgxModule(new StringGeneratingTemplateFactory());
+		case "eml": return new EmlModule();
 		default: return new EolModule();
 		}
 	}
@@ -168,7 +212,7 @@ public class RunEpsilonFunction extends EpsilonLiveFunction {
 				"Tree [%=t.name%]", 
 				"<?nsuri tree?><_><tree name=\"t1\"/><tree name=\"t2\"/></_>",
 				"package tree; class Tree { attr String name; }", 
-				"", "", System.out, response);
+				"", "", "", "", System.out, response);
 		
 		System.out.println(response.get("generatedFiles"));
 		
